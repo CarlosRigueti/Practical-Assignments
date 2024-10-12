71536b064f2ad33515f845805aa62cf0f474e6da
Carlos Rigueti <carlos.rigueti@gmail.com>
2024-10-12 15:05:40 +0100

    datalab01.py

--- datalab01.py
+++ datalab01.py
@@ -4,7 +4,7 @@ FILENAME= "data.csv"
 DATADIR = "Lab01"
 
 
-with open (DATADIR + FILENAME, "rt") as fp:
+with open ("data.csv") as fp:
 reader = csv.reader(fp, delimiter=",")
 for line in reader:
 print (line)
